e4bac615acd2d72a0566fc4d22433af70aa2b11f
Buihuong1203 <huongbtph16610@fpt.edu.vn>
2022-07-31 23:11:50 +0700

    push lại

--- pages/admin/user/index.tsx
+++ pages/admin/user/index.tsx
@@ -24,23 +24,7 @@ const AdminPageUser = (props: Props) => {
             <tbody className="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
               <tr className="bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-900 text-gray-700 dark:text-gray-400">
                 <td className="px-4 py-3">
-                  <div className="flex items-center text-sm">
-                    <div className="relative hidden w-8 h-8 mr-3 rounded-full md:block">
-                      <img
-                        className="object-cover w-full h-full rounded-full"
-                        src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
-                        alt=""
-                        loading="lazy"
-                      />
-                      <div
-                        className="absolute inset-0 rounded-full shadow-inner"
-                        aria-hidden="true"
-                      />
-                    </div>
                     <td className="px-4 py-3 text-sm">12345678</td>
-                      
-                      
-                  </div>
                 </td>
                 <td><p className="font-semibold">ADMIN 1</p></td>
                 <td className="px-4 py-3 text-sm">admin1@gmail.com</td>
