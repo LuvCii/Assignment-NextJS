67e46af8da22933609090cc64b956cc52d106e01
Trần Minh Dũng <tranminhdungggg@gmail.com>
2022-08-04 21:08:15 +0700

    Update package.json

---
+++ hooks/use-auth.tsx
@@ -0,0 +1,28 @@
+import { listUser, removeUser, updateUser } from "../api/user";
+import useSWR from "swr";
+
+const useAuth = (option? :any) => {
+    const {data, error, mutate} = useSWR("/user");
+
+    //delete
+    const remove = async (_id :any) => {
+        if(window.confirm("Bạn có chắc chắn muốn xóa không ?")){
+            await removeUser(_id);
+            const newUsers = data.filter((item: any) => item.id != _id);
+            mutate(newUsers);
+        }
+        
+    };
+    //update
+    const update = async(user: any) => {
+        await updateUser(user)
+        const newUser = data.map((item:any) => item._id === data._id ? user :item)
+    }
+    return {
+        data,
+        error,
+        remove,
+        update
+    }
+}
+export default useAuth
