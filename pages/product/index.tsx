1415e80c85c6e24c52cc38b01a575712bb40afdb
thao02122002 <vant16039@gmail.com>
2022-08-04 22:03:39 +0700

    GROUP8-63 fix bug ui productPage

--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -27,96 +27,67 @@ const ProductPage = (props: Props) => {
               <li className='inline-block font-bold font-sans hover:text-red-700'><Link href="">Uncategorized</Link></li>
             </nav>
           </div>
-          <div className='py-8'>
-            <div className='flex justify-between'>
-            <div className='col-span-2'> 
-            <h1 className='text-2xl font-bold font-serif'>AUTHOR</h1>
-            </div>
-            <div className='col-span-2'>
-               <img src='https://cdn-icons-png.flaticon.com/512/134/134210.png' width="16px"/>
-               </div>
-               </div>
-            <hr className='py-2'></hr>
-            <nav className=''>
-              <li className='inline-block font-bold font-sans hover:text-red-700'><input className='border rounded-lg border-black p-1' type="checkbox" /> <Link  href="">Drama</Link></li><br></br>
-              <li className='inline-block font-bold font-sans hover:text-red-700'><input className='border rounded-lg border-black p-1' type="checkbox" /> <Link href="">Mystery</Link></li><br></br>
-              <li className='inline-block font-bold font-sans hover:text-red-700'><input className='border rounded-lg border-black p-1' type="checkbox" /> <Link href="">Novels</Link></li><br></br>
-              <li className='inline-block font-bold font-sans hover:text-red-700'><input className='border rounded-lg border-black p-1' type="checkbox" /> <Link href="">Recipe</Link></li><br></br>
-              <li className='inline-block font-bold font-sans hover:text-red-700'><input className='border rounded-lg border-black p-1' type="checkbox" /> <Link href="">Uncatego</Link></li>
-            </nav>
-           
           
-          
-          </div>
-          <div className=''>
-            <div className='flex justify-between'>
-            <div className='col-span-2'> 
-            <h1 className='text-2xl font-bold font-serif'>PRICE</h1>
-            </div>
-            <div className='col-span-2'> 
-            <img src='https://cdn-icons-png.flaticon.com/512/134/134210.png' width="16px"/>
-            </div>
-            </div>
-            <hr className='py-2'></hr>
-            <nav className=''>
-              <li className='inline-block font-bold font-sans hover:text-red-700'><input className='border rounded-lg border-black p-1' type="checkbox" /> <Link  href="">50-100</Link></li><br></br>
-              <li className='inline-block font-bold font-sans hover:text-red-700'><input className='border rounded-lg border-black p-1' type="checkbox" /> <Link href="">100-150</Link></li><br></br>
-              <li className='inline-block font-bold font-sans hover:text-red-700'><input className='border rounded-lg border-black p-1' type="checkbox" /> <Link href="">150-200</Link></li><br></br>
-              <li className='inline-block font-bold font-sans hover:text-red-700'><input className='border rounded-lg border-black p-1' type="checkbox" /> <Link href="">200-300</Link></li><br></br>
-              <li className='inline-block font-bold font-sans hover:text-red-700'><input className='border rounded-lg border-black p-1' type="checkbox" /> <Link href="">300-500</Link></li>
-            </nav>
-           
-          
-          
-          </div>
 
           
           
         </div>
-        <div className='col-span-9 grid grid-cols-8 gap-2'>
-          <div className='col-span-2 text-center pb-2 px-3'>
-          <div className='image overflow-hidden '>
-                <img className='transition-transform duration-500 transform hover:scale-125' src='https://booklovers.ancorathemes.com/wp-content/uploads/2020/05/book12-copyright-400x600.jpg'  />
+        <div className='col-span-9 '>
+        <div className='grid grid-cols-8 gap-2 justify-around flex-wrap'>
+            <div className="col-span-2 my-8 group relative hover:shadow-lg hover:shadow-zinc-600 hover:translate-y-[-15px] duration-500 ease-in-out transition-all bg-white rounded-md overflow-hidden">
+              <div className='z-30 translate-x-[-35px] delay-100 group-hover:translate-x-[0px] ease-in-out transition-all ml-1 mt-1 absolute shadow-inner rounded-lg bg-zinc-300'>
+                <Link href="/product">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="hover:text-white hover:bg-red-500 rounded-lg h-7 w-7 text-slate-400 p-[2px]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                  </svg>
+                </Link>
+
               </div>
-              <div className='content'>
-                <h4 className='text-2xl font-bold hover:text-red-500'>Converse Chuck Taylor All Star SP OX</h4>
-                <p className='text-xl font-serif'>Burt Geller</p>
-                <span className='text-red-500 text-2xl font-bold'>1.500.000đ</span>  <del>1.800.000đ</del><br></br>
-                
+
+              <div className='z-30 translate-x-[-35px] delay-150 group-hover:translate-x-[0px] ease-in-out transition-all ml-1 mt-1 absolute top-[33px] shadow-inner rounded-lg bg-zinc-300'>
+                <Link href="/product">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="hover:text-white hover:bg-red-500 rounded-lg h-7 w-7 text-slate-400 p-[3px]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                  </svg>
+                </Link>
+
               </div>
-          </div>
-          <div className='col-span-2 text-center pb-2 px-3'>
-          <div className='image overflow-hidden '>
-                <img className='transition-transform duration-500 transform hover:scale-125' src='https://booklovers.ancorathemes.com/wp-content/uploads/2020/05/book12-copyright-400x600.jpg'  />
+
+              <div className='z-30 translate-x-[-35px] delay-200 group-hover:translate-x-[0px] ease-in-out transition-all ml-1 mt-1 absolute top-[66px] shadow-inner rounded-lg bg-zinc-300'>
+                <Link href="/product">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="hover:text-white hover:bg-red-500 rounded-lg h-7 w-7 text-slate-400 p-[2px]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                  </svg>
+                </Link>
+
               </div>
-              <div className='content'>
-                <h4 className='text-2xl font-bold hover:text-red-500'>Converse Chuck Taylor All Star SP OX</h4>
-                <p className='text-xl font-serif'>Burt Geller</p>
-                <span className='text-red-500 text-2xl font-bold'>1.500.000đ</span>  <del>1.800.000đ</del><br></br>
-                
+
+              <div className="w-full min-h-80 bg-white aspect-w-1 aspect-h-1 overflow-hidden  lg:h-80 lg:aspect-none">
+                <img src="https://bookbuy.vn/Res/Images/Product/9-mau-chia-ly_117801_1.PNG" alt="" className="rounded-md px-12 pt-8 pb-4 overflow-hidden grayscale group-hover:grayscale-0  group-hover:scale-110 w-full h-full object-center object-cover ease-in-out transform transition-all lg:w-full lg:h-full" />
               </div>
-          </div>
-          <div className='col-span-2 text-center pb-2 px-3'>
-          <div className='image overflow-hidden '>
-                <img className='transition-transform duration-500 transform hover:scale-125' src='https://booklovers.ancorathemes.com/wp-content/uploads/2020/05/book12-copyright-400x600.jpg'  />
+
+              <div className="mt-1 ">
+                <div>
+                  <h3 className="text-base my-1 text-gray-800 font-medium text-center">
+                    <span aria-hidden="true" className="absolute inset-0"></span>
+                    9 màu chia ly
+                  </h3>
+                </div>
+                <div className='mb-2 flex justify-around'>
+                  <div><del className='text-red-500'><p className="mt-1 italic text-sm text-red-500">200 000 VNĐ</p></del></div>
+                  <div><p className="text-base font-medium italic text-gray-500">150 000 VNĐ</p></div>
+                </div>
+
               </div>
-              <div className='content'>
-                <h4 className='text-2xl font-bold hover:text-red-500'>Converse Chuck Taylor All Star SP OX</h4>
-                <p className='text-xl font-serif'>Burt Geller</p>
-                <span className='text-red-500 text-2xl font-bold'>1.500.000đ</span>  <del>1.800.000đ</del><br></br>
-                
-              </div>
-          </div>
-          <div className='col-span-2 text-center pb-2 px-3'>
-          <div className='image overflow-hidden '>
-                <img className='transition-transform duration-500 transform hover:scale-125' src='https://booklovers.ancorathemes.com/wp-content/uploads/2020/05/book12-copyright-400x600.jpg'  />
-              </div>
-              <div className='content'>
-                <h4 className='text-2xl font-bold hover:text-red-500'>Converse Chuck Taylor All Star SP OX</h4>
-                <p className='text-xl font-serif'>Burt Geller</p>
-                <span className='text-red-500 text-2xl font-bold'>1.500.000đ</span>  <del>1.800.000đ</del><br></br>
-                
-              </div>
+            </div>
+            
+            
+           
+            
+            
+            
+
           </div>
         </div>
       </div>
