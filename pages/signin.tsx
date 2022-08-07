7bd874f09cbb48b5b4f2f2ba376a632704304788
Trần Minh Dũng <tranminhdungggg@gmail.com>
2022-08-07 13:08:32 +0700

    GROUP8-71 validate field data

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -61,13 +61,17 @@ const SignIn = () => {
                   Email address
                 </label>
                 <input
-                  {...register("email")}
+                  {...register("email",{ required: true, pattern: /^\S+@\S+$/i })}
                   type="email"
                   autoComplete="email"
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Email address"
                 />
+                 <p className='text-left pl-4 text-red-500 text-base italic'>
+                  {errors.email?.type === 'required' && "Vui lòng điền vào trường này"}
+                  {errors.email?.type === 'pattern' && "Vui lòng nhập đúng định dạng email"}
+              </p>
               </div>
               <br />
               <div>
